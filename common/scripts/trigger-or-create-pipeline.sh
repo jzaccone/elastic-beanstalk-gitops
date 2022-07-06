b292c7eed0fe02a40a2fbba04cd32324922bc4c0
John Zaccone <john.zaccone@gmail.com>
2022-07-06 06:28:49 -0400

    test change

--- common/scripts/trigger-or-create-pipeline.sh
+++ common/scripts/trigger-or-create-pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for DIR_NAME in $(ls -d */| sed 's:/*$::') ; do
-  # Skip the main branch with common resources
-  if [ "$DIR_NAME" = "main" ]; then
+  # Skip the common branch with common resources
+  if [ "$DIR_NAME" = "common" ]; then
     continue
   fi
 
@@ -26,17 +26,17 @@ for DIR_NAME in $(ls -d */| sed 's:/*$::') ; do
       echo "The pipeline $PIPELINE_NAME does not exist, creating it..."
 
       PIPELINE_CONFIG_COPY="pipeline-config-${DIR_NAME}.json"
-      cp main/pipeline-config.json $PIPELINE_CONFIG_COPY
+      cp common/pipeline-config.json $PIPELINE_CONFIG_COPY
 
       sed -i '' "s@PIPELINE_NAME@$PIPELINE_NAME@g" $PIPELINE_CONFIG_COPY
-      sed -i '' "s@STACK_TEMPLATE_FILE@main/cloudformation.yaml@g" $PIPELINE_CONFIG_COPY
+      sed -i '' "s@STACK_TEMPLATE_FILE@common/cloudformation.yaml@g" $PIPELINE_CONFIG_COPY
       sed -i '' "s@STACK_CONFIG_FILE@${DIR_NAME}/stack-config.json@g" $PIPELINE_CONFIG_COPY
       sed -i '' "s@STACK_NAME@spring-eb-${DIR_NAME}@g" $PIPELINE_CONFIG_COPY
 
       echo "Pipeline Config:"
       cat $PIPELINE_CONFIG_COPY
 
-      aws cloudformation create-stack --stack-name ${PIPELINE_NAME}-pipeline --template-body "file://$(pwd)/main/pipeline.yaml" --parameters "file://$(pwd)/${PIPELINE_CONFIG_COPY}" --capabilities CAPABILITY_NAMED_IAM 
+      aws cloudformation create-stack --stack-name ${PIPELINE_NAME}-pipeline --template-body "file://$(pwd)/common/pipeline.yaml" --parameters "file://$(pwd)/${PIPELINE_CONFIG_COPY}" --capabilities CAPABILITY_NAMED_IAM 
       echo "A CFT to create the stack has been created. The pipeline will trigger automatically after creation."
     else
       echo "The pipeline $PIPELINE_NAME exists, triggering the pipeline!"
